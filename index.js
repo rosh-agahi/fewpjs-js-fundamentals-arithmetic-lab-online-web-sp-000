086019339c9f2f0c70b3eb412085f9ed974bc4f8
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 05:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let num1 = 2;
 let num2 = 31;
 const multiply = num1 * num2;
 
-const random = Math.floor(Math.random() * 5) + 1;
+const random = Math.ceiling(Math.random() * 7);
 
 let num3 = 10;
 let num4 = 6;
 
 const mod = num3 % num4;
 
-const max = Math.max(5, 10, 12, 16, 20);
+const max = Math.max(5, 10, 15, 20);
