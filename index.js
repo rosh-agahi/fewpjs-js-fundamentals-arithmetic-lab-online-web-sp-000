afaca115848e648d4174bc65455a5e1fbb1a8ae4
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 05:42:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 5) + 1;
+
+let num3 = 10;
+let num4 = 6;
+
+const mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
